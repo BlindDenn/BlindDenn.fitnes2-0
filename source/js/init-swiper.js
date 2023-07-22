ceb3edd12a875bd07c8f7f20b28f0ed2ab05f92b
BlindDenn <blinddenn@gmail.com>
2023-07-22 23:48:47 +0300

    starts working on swiper-arrows

--- source/js/init-swiper.js
+++ source/js/init-swiper.js
@@ -2,13 +2,36 @@ import Swiper from './vendor/swiper';
 
 const initSwiper = () => {
   var swiper = new Swiper(".swiper", {
-    slidesPerView: 4,
-    spaceBetween: 40,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      556: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1066: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      // 1500: {
+      //   slidesPerView: 5,
+      //   spaceBetween: 40,
+      // },
+    },
     grabCursor: true,
     autoHeight: false,
+    loop: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".trainers__swiper-button-next",
+      prevEl: ".trainers__swiper-button-prev",
     },
   });
 };
