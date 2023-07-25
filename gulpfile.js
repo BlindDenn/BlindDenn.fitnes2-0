df0431c6734fd44511cae69debfa49e4153e7fae
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-07-25 23:21:37 +0300

    adds Offers desktop

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import del from 'del';
-import {compileStyles, compileMinStyles} from './gulp/compileStyles.mjs';
+import { stacksvg } from "gulp-stacksvg"
+import { compileStyles, compileMinStyles } from './gulp/compileStyles.mjs';
 import { copy, copyImages, copySvg } from './gulp/copyAssets.mjs';
-import {compileMainMinScripts, compileMainScripts, compileVendorScripts} from './gulp/compileScripts.mjs';
-import {optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg} from './gulp/optimizeImages.mjs';
+import { compileMainMinScripts, compileMainScripts, compileVendorScripts } from './gulp/compileScripts.mjs';
+import { optimizeSvg, createWebp, optimizePng, optimizeJpg } from './gulp/optimizeImages.mjs';
+// import {optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg} from './gulp/optimizeImages.mjs';
 
 const server = browserSync.create();
 const streamStyles = () => compileStyles().pipe(server.stream());
 
 const clean = () => del('build');
+
+const { src, dest } = gulp
+
+const makeStack = () => {
+  return src(`./source/img/sprite/*.svg`)
+    .pipe(stacksvg({ output: `sprite` }))
+    .pipe(dest(`./build/img`))
+}
 
 const syncServer = () => {
   server.init({
@@ -25,7 +35,8 @@ const syncServer = () => {
   gulp.watch('source/sass/**/*.{scss,sass}', streamStyles);
   gulp.watch('source/js/**/*.{js,json}', gulp.series(compileMainScripts, compileVendorScripts, refresh));
   gulp.watch('source/data/**/*.{js,json}', gulp.series(copy, refresh));
-  gulp.watch('source/img/**/*.svg', gulp.series(copySvg, sprite, refresh));
+  gulp.watch('source/img/**/*.svg', gulp.series(copySvg, refresh));
+  gulp.watch('source/img/sprite/*.svg', makeStack());
   gulp.watch('source/img/**/*.{png,jpg,webp}', gulp.series(copyImages, refresh));
 
   gulp.watch('source/favicon/**', gulp.series(copy, refresh));
@@ -39,8 +50,8 @@ const refresh = (done) => {
   done();
 };
 
-const build = gulp.series(clean, copy, sprite, gulp.parallel(compileMinStyles, compileMainMinScripts, compileVendorScripts, optimizePng, optimizeJpg, optimizeSvg));
-const dev = gulp.series(clean, copy, sprite, gulp.parallel(compileMinStyles, compileMainMinScripts, compileVendorScripts, optimizePng, optimizeJpg, optimizeSvg), syncServer);
-const start = gulp.series(clean, copy, sprite, gulp.parallel(compileStyles, compileMainScripts, compileVendorScripts), syncServer);
+const build = gulp.series(clean, copy, makeStack, gulp.parallel(compileMinStyles, compileMainMinScripts, compileVendorScripts, optimizePng, optimizeJpg, optimizeSvg));
+const dev = gulp.series(clean, copy, makeStack, gulp.parallel(compileMinStyles, compileMainMinScripts, compileVendorScripts, optimizePng, optimizeJpg, optimizeSvg), syncServer);
+const start = gulp.series(clean, copy, makeStack, gulp.parallel(compileStyles, compileMainScripts, compileVendorScripts), syncServer);
 
-export { createWebp as webp, build, start, dev};
+export { createWebp as webp, build, start, dev };
