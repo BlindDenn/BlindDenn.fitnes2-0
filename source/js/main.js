5f00e2ecd2351ac2e44fbf977b5391c57a4a8a43
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-07-31 20:46:26 +0300

    clears main.js

--- source/js/main.js
+++ source/js/main.js
@@ -1,26 +1,11 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
-import {initVideo} from './video';
-import {initTabs} from './vendor/tabs/init-tabs';
-import {initAccordions} from './vendor/accordion/init-accordion';
-import {initTrainersSwiper} from './init-trainers-swiper';
-import {initReviewsSwiper} from './init-reviews-swiper';
-
-
-window.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('load', () => {
-    initTabs();
-  });
-});
-
-
-window.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('load', () => {
-    initAccordions();
-  });
-});
-
+import { iosVhFix } from './utils/ios-vh-fix';
+import { initModals } from './modules/modals/init-modals';
+import { Form } from './modules/form-validate/form';
+import { initVideo } from './video';
+import { initTabs } from './vendor/tabs/init-tabs';
+import { initAccordions } from './vendor/accordion/init-accordion';
+import { initTrainersSwiper } from './init-trainers-swiper';
+import { initReviewsSwiper } from './init-reviews-swiper';
 
 // ---------------------------------
 
@@ -31,12 +16,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
   iosVhFix();
 
+  initVideo();
+  initTrainersSwiper();
+  initReviewsSwiper();
+
   // Modules
   // ---------------------------------
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    initTabs();
+    initAccordions();
     initModals();
     const form = new Form();
     window.form = form;
@@ -44,9 +35,6 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-initVideo();
-initTrainersSwiper();
-initReviewsSwiper();
 
 // ---------------------------------
 
